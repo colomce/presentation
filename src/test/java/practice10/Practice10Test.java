09a59d40662a8e825de5db79bedd5e1254c1e894
Cedric Don Coloma <colomce@oocl.com>
2020-10-14 21:33:18 +0800

    [PRACTICE10][Cedric Coloma] feature: Added Student introduce implementation

--- src/test/java/practice10/Practice10Test.java
+++ src/test/java/practice10/Practice10Test.java
@@ -80,20 +80,20 @@ public class Practice10Test {
         assertThat(tom.getKlass()).isEqualTo(klass2);
     }
 
-//    @Test
-//    public void should_student_introduce_with_class() throws Exception {
-//        Student tom = new Student(1, "Tom", 21, klass2);
-//        assertThat(tom.introduce()).isEqualTo("My name is Tom. I am 21 years old. I am a Student. I am at Class 2.");
-//    }
-//
-//    @Test
-//    public void should_student_introduce_itself_as_class_leader() throws Exception {
-//        Student tom = new Student(1, "Tom", 21, klass2);
-//        klass2.appendMember(tom);
-//        klass2.assignLeader(tom);
-//        assertThat(tom.introduce()).isEqualTo("My name is Tom. I am 21 years old. I am a Student. I am Leader of Class 2.");
-//    }
-//
+    @Test
+    public void should_student_introduce_with_class() throws Exception {
+        Student tom = new Student(1, "Tom", 21, klass2);
+        assertThat(tom.introduce()).isEqualTo("My name is Tom. I am 21 years old. I am a Student. I am at Class 2.");
+    }
+
+    @Test
+    public void should_student_introduce_itself_as_class_leader() throws Exception {
+        Student tom = new Student(1, "Tom", 21, klass2);
+        klass2.appendMember(tom);
+        klass2.assignLeader(tom);
+        assertThat(tom.introduce()).isEqualTo("My name is Tom. I am 21 years old. I am a Student. I am Leader of Class 2.");
+    }
+
 //    @Test
 //    public void should_teacher_have_name_and_age_and_classes() throws Exception {
 //        LinkedList<practice10.Klass> linkedList = new LinkedList<Klass>();
