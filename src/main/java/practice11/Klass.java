ffce386bb940337635b78f36730777fe0fb6a7b4
Cedric Don Coloma <colomce@oocl.com>
2020-10-14 22:50:22 +0800

    [PRACTICE11][Cedric Coloma] feature: Make class implement my Observable

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,11 +1,16 @@
 package practice11;
 
-public class Klass {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Klass implements Observable {
     private int number;
     private Student leader;
+    private List<Observer> observerList;
 
     public Klass(int number) {
         this.number = number;
+        observerList = new ArrayList<>();
     }
 
     public int getNumber() {
@@ -33,10 +38,31 @@ public class Klass {
             System.out.print("It is not one of us.\n");
         } else {
             this.leader = student;
+            notifyObservers(student, "leader");
         }
     }
 
     public void appendMember(Student student) {
         student.setKlass(this);
+        notifyObservers(student, "append");
+    }
+
+    @Override
+    public void registerObserver(Observer observer) {
+        if(observer != null) {
+            this.observerList.add(observer);
+        }
+    }
+
+    @Override
+    public void notifyObservers(Student student, String action) {
+        this.observerList.stream().forEach(observer ->  {observer.update(student, this, action);});
+    }
+
+    @Override
+    public void removeObserver(Observer observer) {
+        if(observer != null ){
+            this.observerList.remove(observer);
+        }
     }
 }
