eb7f425b06ef8696e6238a30a9af0da861818adb
Cedric Don Coloma <colomce@oocl.com>
2020-10-14 20:16:33 +0800

    [PRACTICE8][Cedric Coloma] feature: Added  introduceWith method that accept student

--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -24,4 +24,10 @@ public class Teacher extends Person {
                 super.introduce() + " I am a Teacher. I teach No Class." :
                 super.introduce() + " I am a Teacher. I teach " + this.klass.getDisplayName() + ".";
     }
+
+    public String introduceWith(Student student) {
+        return student.getKlass().getNumber() == this.klass.getNumber() ?
+                super.introduce() + " I am a Teacher. I teach " + student.getName() + "." :
+                "";
+    }
 }
