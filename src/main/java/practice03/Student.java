354e4a67c69116a5e00b988f69e2cbc2fd1c7f4c
Cedric Don Coloma <colomce@oocl.com>
2020-10-14 15:54:35 +0800

    [PRACTICE3][Cedric Coloma] feature: Added Student class that inherits from Person and added access method to pass should_student_have_name_age_and_class_number

--- src/main/java/practice03/Student.java
+++ src/main/java/practice03/Student.java
@@ -1,4 +1,18 @@
 package practice03;
 
-public class Student {
+public class Student extends Person{
+    private int Klass;
+
+    public Student(String name, int age, int Klass) {
+        super(name, age);
+        this.Klass = Klass;
+    }
+
+    public int getKlass() {
+        return Klass;
+    }
+
+    public void setKlass(int klass) {
+        Klass = klass;
+    }
 }
