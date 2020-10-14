3b781b5e58704d48ab13ba08b5091570b5494d71
Cedric Don Coloma <colomce@oocl.com>
2020-10-14 21:33:50 +0800

    [PRACTICE10][Cedric Coloma] feature: Added Teacher that accept linkedlist of classes

--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,22 @@
 package practice10;
+import java.util.LinkedList;
 
-public class Teacher {
+public class Teacher extends Person {
+    private LinkedList<Klass> klasses;
+    public Teacher(int id, String name, int age, LinkedList<Klass> klasses) {
+        super(id, name, age);
+        this.klasses = klasses;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public LinkedList<Klass> getClasses() {
+        return klasses;
+    }
+
+    public void setClasses(LinkedList<Klass> klasses) {
+        this.klasses = klasses;
+    }
 }
