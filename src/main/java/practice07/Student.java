83f82a56d01f6a695a65e285dfd1db76334813be
Cedric Don Coloma <colomce@oocl.com>
2020-10-14 19:09:07 +0800

    [PRACTICE7][Cedric Coloma] feature: Added introduce method in Student class which uses the display name in klass.

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -15,4 +15,8 @@ public class Student extends Person{
     public void setKlass(Klass klass) {
         this.klass = klass;
     }
+
+    public String introduce()    {
+        return super.introduce() + " I am a Student. I am at " + klass.getDisplayName() + ".";
+    }
 }
