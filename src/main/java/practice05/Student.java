001babc5cbe89cffc7442eda3465a706c3930987
Cedric Don Coloma <colomce@oocl.com>
2020-10-14 16:16:24 +0800

    [PRACTICE5][Cedric Coloma] feature: Added Student class that inherits from Person with additional Klass property and accessor methods.

--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,17 @@
 package practice05;
 
-public class Student {
+public class Student extends Person {
+    private int Klass;
+    public Student(String name, int age, int Klass) {
+        super(name, age);
+        this.Klass = Klass;
+    }
+
+    public int getKlass() {
+        return Klass;
+    }
+
+    public void setKlass(int klass) {
+        Klass = klass;
+    }
 }
