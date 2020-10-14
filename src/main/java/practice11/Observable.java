53f4c4c9f99d71aee0d48d2c69bc9228104f721e
Cedric Don Coloma <colomce@oocl.com>
2020-10-14 22:49:52 +0800

    [PRACTICE11][Cedric Coloma] feature: Added observer and observable interface

---
+++ src/main/java/practice11/Observable.java
@@ -0,0 +1,7 @@
+package practice11;
+
+public interface Observable {
+    public void registerObserver(Observer observer);
+    public void notifyObservers(Student student, String action);
+    public void removeObserver(Observer observer);
+}
---
+++ src/main/java/practice11/Observer.java
@@ -0,0 +1,5 @@
+package practice11;
+
+public interface Observer {
+    void update(Student student, Klass klass, String type);
+}
