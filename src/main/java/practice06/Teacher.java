127f88b33937b6b5743234876ce01908916a61cd
Cedric Don Coloma <colomce@oocl.com>
2020-10-14 17:06:23 +0800

    [PRACTICE6][Cedric Coloma] feature: Added Teacher with klass

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,17 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private int klass;
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
 }
