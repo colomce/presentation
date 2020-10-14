2476950e8433202f83ba8059d2c146e4a31fa020
Cedric Don Coloma <colomce@oocl.com>
2020-10-14 21:08:32 +0800

    [PRACTICE9][Cedric Coloma] feature: Added Teacher Class

--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,17 @@
 package practice09;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
 }
