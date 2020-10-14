fd464d037a66335c9f09c399639235e56fc19214
Cedric Don Coloma <colomce@oocl.com>
2020-10-14 21:59:37 +0800

    [PRACTICE11][Cedric Coloma] feature: Added teacher instantiation implementations

--- src/main/java/practice11/Teacher.java
+++ src/main/java/practice11/Teacher.java
@@ -1,4 +1,24 @@
 package practice11;
 
-public class Teacher {
+import java.util.LinkedList;
+
+public class Teacher extends Person{
+    private LinkedList<Klass> klasses;
+    public Teacher(int id, String name, int age, LinkedList<Klass> klasses) {
+        super(id, name, age);
+        this.klasses = klasses;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public LinkedList<Klass> getClasses() {
+        return klasses;
+    }
+
+    public void setClasses(LinkedList<Klass> klasses) {
+        this.klasses = klasses;
+    }
+
 }
