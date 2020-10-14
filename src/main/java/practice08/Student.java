00407496cb8fd4fbb262f2ba835b6177cf7d2de4
Cedric Don Coloma <colomce@oocl.com>
2020-10-14 19:46:48 +0800

    [PRACTICE8][Cedric Coloma] feature: Added introduce in Student Class

--- src/main/java/practice08/Student.java
+++ src/main/java/practice08/Student.java
@@ -14,4 +14,8 @@ public class Student extends Person {
     public void setKlass(Klass klass) {
         this.klass = klass;
     }
+
+    public String introduce()    {
+        return super.introduce() + " I am a Student. I am at " + klass.getDisplayName() + ".";
+    }
 }
