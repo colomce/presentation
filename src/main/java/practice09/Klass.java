28c35ced2cfc44d9cb73480c1f98aba79ef40117
Cedric Don Coloma <colomce@oocl.com>
2020-10-14 20:34:56 +0800

    [PRACTICE9][Cedric Coloma] feature: Added Klass getDisplayName method

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -17,4 +17,8 @@ public class Klass {
     public void setNumber(int number) {
         this.number = number;
     }
+
+    public String getDisplayName() {
+        return "Class " + this.number;
+    }
 }
