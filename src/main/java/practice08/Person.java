3f16d94786e6c32b7be0e279529369a00990856a
Cedric Don Coloma <colomce@oocl.com>
2020-10-14 19:35:08 +0800

    [PRACTICE8][Cedric Coloma] feature: Override equals and hashCode method in Person class

--- src/main/java/practice08/Person.java
+++ src/main/java/practice08/Person.java
@@ -36,4 +36,17 @@ public class Person {
     public void setAge(int age) {
         this.age = age;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
